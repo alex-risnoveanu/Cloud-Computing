c0efceab60dacdfb4d3084e1f0af4c8fe5cbe3da
Alex Risnoveanu <alex.max91@yahoo.com>
2020-04-11 17:21:01 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using System;
+using Generated;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Host = "localhost";
+            const int Port = 16842;
+
+            var channel = new Channel($"{Host}:{Port}", ChannelCredentials.Insecure);
+
+            var request = new HalloRequest();
+
+            Console.WriteLine("Enter you'r name: ");
+
+            request.Name = Console.ReadLine();
+
+            var client = new HalloService.HalloServiceClient(channel);
+
+            client.SayHi(request);
+
+            // Shutdown
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/HalloService.cs
@@ -0,0 +1,16 @@
+﻿using Generated;
+using System;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class HalloService : Generated.HalloService.HalloServiceBase
+    {
+        public override Task<HalloResponse> SayHi(HalloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Hello "+request.Name);
+            return Task.FromResult(new HalloResponse());
+        }
+    }
+}
